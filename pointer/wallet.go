9dc1241e70474788c1b3f966720a25e39dc6c78d
baorongquan <brq0122@163.com>
2018-07-22 16:12:54 +0800

    add Deposit and Balance function

---
+++ pointer/wallet.go
@@ -0,0 +1,25 @@
+package pointer
+
+import "fmt"
+
+type Bitcoin int
+type Wallet struct {
+	balance Bitcoin
+}
+
+func (w *Wallet) Deposit(amount Bitcoin) {
+	fmt.Println("address of balance in Deposit is ", &w.balance)
+	w.balance += amount
+}
+
+func (w *Wallet) Balance() Bitcoin {
+	return w.balance
+}
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
---
+++ pointer/wallet_test.go
@@ -0,0 +1,19 @@
+package pointer
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+	wallet.Deposit(Bitcoin(10))
+
+	got := wallet.Balance()
+
+	fmt.Println("address of balance in test is ", &wallet.balance)
+	want := Bitcoin(10)
+	if got != want {
+		t.Errorf("got '%s' want '%s'", got, want)
+	}
+}
