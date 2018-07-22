698bf1267cd1d87cb66bd382449d411221e21de6
baorongquan <brq0122@163.com>
2018-07-22 15:41:13 +0800

    add Table Driven Test

--- struct_mothed_interface/shape_test.go
+++ struct_mothed_interface/shape_test.go
@@ -34,4 +34,20 @@ func TestArea(t *testing.T) {
 		want := 314.1592653589793
 		checkArea(t, circle, want)
 	})
+
+	aresTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{Height: 12.0, Width: 6.0}, want: 72.0},
+		{shape: Circle{Radius: 10.0}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12.0, Height: 6.0}, want: 36.0},
+	}
+
+	for _, tt := range aresTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %.2f want %.2f", got, tt.want)
+		}
+	}
 }
