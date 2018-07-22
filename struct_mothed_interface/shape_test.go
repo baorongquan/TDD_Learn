23a52531b117fe926bc8cf9f4ead9a1ef1d22875
baorongquan <brq0122@163.com>
2018-07-22 15:50:04 +0800

    add name to every test

--- struct_mothed_interface/shape_test.go
+++ struct_mothed_interface/shape_test.go
@@ -36,18 +36,21 @@ func TestArea(t *testing.T) {
 	})
 
 	aresTests := []struct {
+		name  string
 		shape Shape
 		want  float64
 	}{
-		{shape: Rectangle{Height: 12.0, Width: 6.0}, want: 72.0},
-		{shape: Circle{Radius: 10.0}, want: 314.1592653589793},
-		{shape: Triangle{Base: 12.0, Height: 6.0}, want: 36.0},
+		{name: "Rectangle", shape: Rectangle{Height: 12.0, Width: 6.0}, want: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, want: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12.0, Height: 6.0}, want: 36.0},
 	}
 
 	for _, tt := range aresTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %.2f want %.2f", got, tt.want)
-		}
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.want {
+				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.want)
+			}
+		})
 	}
 }
