002f7c5f59bcae495c31c7fb5328597986c36fba
baorongquan <brq0122@163.com>
2018-07-22 16:23:37 +0800

    add Withdraw function

--- pointer/wallet_test.go
+++ pointer/wallet_test.go
@@ -1,19 +1,30 @@
 package pointer
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(Bitcoin(10))
-
-	got := wallet.Balance()
-
-	fmt.Println("address of balance in test is ", &wallet.balance)
-	want := Bitcoin(10)
-	if got != want {
-		t.Errorf("got '%s' want '%s'", got, want)
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got '%s' want '%s'", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(5))
+
+		assertBalance(t, wallet, Bitcoin(15))
+	})
 }
