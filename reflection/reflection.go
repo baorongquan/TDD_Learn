a5f9515abd9def4dd9e847dc8a2cd163718cf0f3
baorongquan <brq0122@163.com>
2018-08-12 17:06:12 +0800

    add struct and slice

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -5,7 +5,33 @@ import (
 )
 
 func walk(x interface{}, fn func(input string)) {
+	val := getValue(x)
+
+	if val.Kind() == reflect.Slice {
+		for i := 0; i < val.Len(); i++ {
+			walk(val.Index(i).Interface(), fn)
+		}
+		return
+	}
+
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
+		}
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
 }
