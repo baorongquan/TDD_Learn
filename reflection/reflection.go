2091c6140fc158632e2133cb5b9009c05829cfbf
baorongquan <brq0122@163.com>
2018-08-12 17:17:28 +0800

    better refactor

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -7,17 +7,22 @@ import (
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
+	numOfValues := 0
+	var getField func(int) reflect.Value
+
 	switch val.Kind() {
-	case reflect.Struct:
-		for i := 0; i < val.NumField(); i++ {
-			walk(val.Field(i).Interface(), fn)
-		}
-	case reflect.Slice:
-		for i := 0; i < val.Len(); i++ {
-			walk(val.Index(i).Interface(), fn)
-		}
 	case reflect.String:
 		fn(val.String())
+	case reflect.Struct:
+		numOfValues = val.NumField()
+		getField = val.Field
+	case reflect.Slice:
+		numOfValues = val.Len()
+		getField = val.Index
+	}
+
+	for i := 0; i < numOfValues; i++ {
+		walk(getField(i).Interface(), fn)
 	}
 }
 
